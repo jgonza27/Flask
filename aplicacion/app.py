b964d635c6a000db8a37bc1a65d10ed5146f4536
Tu Nombre <tu_correo@ejemplo.com>
2026-01-27 10:47:03 +0100

    a

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from aplicacion import config
 from aplicacion.models import Articulos, Categorias, db
+from aplicacion.forms import formArticulo, formCategoria
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -41,3 +42,41 @@ def page_not_found(error):
         "error.html",
         error="Página no encontrada..."
     ), 404
+
+@app.route('/articulos/new', methods=["get", "post"])
+def articulos_new():
+    form = formArticulo()
+    # Consultamos las categorías para el desplegable del formulario
+    categorias = [(c.id, c.nombre) for c in Categorias.query.all()]
+    form.CategoriaId.choices = categorias
+    
+    if form.validate_on_submit():
+        try:
+            f = form.photo.data
+            nombre_fichero = secure_filename(f.filename)
+            f.save(app.root_path + "/static/img/" + nombre_fichero)
+        except:
+            nombre_fichero = ""
+            
+        art = Articulos()
+        form.populate_obj(art)
+        art.image = nombre_fichero
+        
+        db.session.add(art)
+        db.session.commit()
+        
+        return redirect(url_for("inicio"))
+    else:
+        return render_template("articulos_new.html", form=form)
+    
+@app.route('/categorias/new', methods=["get", "post"])
+def categorias_new():
+    form=formCategoria(request.form)
+    if form.validate_on_submit():
+        cat=Categorias(nombre=form.nombre.data)
+        db.session.add(cat)
+        db.session.commit()
+        return redirect(url_for("categorias"))
+    else:
+        return render_template("categorias_new.html",form=form)
+    
---
+++ aplicacion/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import DecimalField, IntegerField, SelectField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
+
+class formArticulo(FlaskForm):
+    nombre = StringField("Nombre:", validators=[DataRequired("Tienes que introducir el dato")])
+    precio = DecimalField("Precio:", default=0, validators=[DataRequired("Tienes que introducir el dato")])
+    iva = IntegerField("IVA:", default=21, validators=[DataRequired("Tienes que introducir el dato")])
+    descripcion = TextAreaField("Descripción:")
+    photo = FileField('Selecciona imagen:')
+    stock = IntegerField("Stock:", default=1, validators=[DataRequired("Tienes que introducir el dato")])
+    CategoriaId = SelectField("Categoría:", coerce=int)
+    submit = SubmitField('Enviar')
+
+class formCategoria(FlaskForm):
+    nombre = StringField("Nombre:", validators=[DataRequired("Tienes que introducir el dato")])
+    submit = SubmitField('Enviar')
